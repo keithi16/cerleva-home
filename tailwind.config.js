20fe34771491741b6e5bbe63e0c41df7a5fee49d
keithi16 <141677778+keithizinn@users.noreply.github.com>
2024-09-09 05:00:29 -0300

    Know Cerleva, projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    // content: [
+    //     "./src/**/*.{zjs,css}",
+    //     "index.html"
+    //     // './*.php',
+    //     // './template-parts/**/*.php',
+    //     // './inc/*.php',
+    // ],
+    // theme: {
+    //   extend: {
+    //     fontFamily: {
+    //         montserrat: ['Montserrat', 'sans-serif']
+    //     },
+    //     colors: {
+    //         'cblack': '#231F20',
+    //       },
+    //     container: {
+    //         screens: {
+    //             '3xl': '105.5rem', 
+    //         },
+    //         padding: {
+    //             '3xl': '2.5rem',
+    //         }
+    //     },
+    //   },
+    // },
+    // plugins: [],
+
     content: [
-        "./src/**/*.{zjs,css}",
-        "index.html"
-        // './*.php',
-        // './template-parts/**/*.php',
-        // './inc/*.php',
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-            montserrat: ['Montserrat', 'sans-serif']
+      "./src/**/*.{js,css}", // Corrigido para 'js' em vez de 'zjs'
+      "index.html"
+      // './*.php',
+      // './template-parts/**/*.php',
+      // './inc/*.php',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+          montserrat: ['Montserrat', 'sans-serif']
+      },
+      colors: {
+          'cblack': '#231F20',
         },
-        colors: {
-            'cblack': '#231F20',
+      container: {
+          center: true, // Centraliza o container
+          screens: {
+              '3xl': '100%', // Corrigido para '100%' em vez de 'w-full'
           },
-        container: {
-            screens: {
-                '3xl': '105.5rem', // 1648px em rem
-            },
-            padding: {
-                '3xl': '2.5rem',
-            }
-        },
+          padding: {
+              '3xl': '10rem',
+          },
       },
     },
-    plugins: [],
+  },
+  plugins: [],
   }
