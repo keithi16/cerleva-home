7f884be40a019e6f96e1788ae58c7ed9857785df
keithi16 <141677778+keithizinn@users.noreply.github.com>
2024-09-16 05:23:35 -0300

    menu 80%

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,26 @@
+const menu = document.getElementById("menu");
+const openButton = document.getElementById("openMenuButton");
+const closeButton = document.getElementById("closeMenuButton");
+
+function toggleMenu() {
+    if (menu.classList.contains("hidden")) {
+        menu.classList.remove("hidden");
+        document.addEventListener("click", closeMenuOnClickOutside);
+    } else {
+        menu.classList.add("hidden");
+        document.removeEventListener("click", closeMenuOnClickOutside);
+    }
+}
+
+function closeMenuOnClickOutside(event) {
+    if (!menu.contains(event.target) && !openButton.contains(event.target) && !closeButton.contains(event.target)) {
+        menu.classList.add("hidden");
+        document.removeEventListener("click", closeMenuOnClickOutside);
+    }
+}
+
+
+
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
